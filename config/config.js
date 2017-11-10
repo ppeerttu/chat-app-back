009b603da6f573c44589ca71f6a47c0165ebb8a9
ppeerttu <perttu.karna@gmail.com>
2017-11-10 12:07:28 +0200

    Testing db on CI

--- config/config.js
+++ config/config.js
@@ -30,5 +30,15 @@ module.exports = {
     password: process.env.POSTGRES_PASSWORD,
     seederStorage: 'sequelize',
     seederStorageTableName: 'SequelizeData'
+  },
+  continous_integration: {
+    host: '127.0.0.1',
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    dialect: 'postgres',
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'SequelizeData'
   }
 };
